e51a64a1503ca37082a8f8433ea932ff18765be3
MuhammadJamshaidGhaffar <muhammadjamshaid89@gmail.com>
2022-07-05 20:40:11 +0500

    Created Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Products from "./Products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="navbar">
+        <NavLink to="Home">Home</NavLink>
+        <NavLink to="products">Products</NavLink>
+      </ul>
+      <Routes>
+        <Route path="/" />
+        <Route path="home" element={<Home />} />
+        <Route path="products" element={<Products />}>
+          <Route index />
+          <Route path=":productId" />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Jamshaid's Shoe Store</h1>
+      <h3>Here you will find best quality Shoes</h3>
+    </div>
+  );
+}
+
+export default Home;
